e90855f308c6cce54147cb93f341a5db8c8a7e21
jyotijoey <jburnwal5@gmail.com>
2021-05-30 18:14:39 +0530

    banner created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Banner from "./Banner";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
             <Switch>
             <Route path={"/"}>
                <Navbar />
+               <Banner />
             </Route>
+
             </Switch>
         </Router>
     </div>
---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+    return (
+        <div className="banner">
+            <img className="banner_image" src="https://www.srentp.com/data1/images/3.jpg" alt="banner" />
+        </div>
+    )
+}
+
+export default Banner
